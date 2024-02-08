6c8abcf9ab95ee8b6920b481d230753c75097ccd
Alex Zhulai <Lepiozdyx@gmail.com>
2024-02-08 09:54:18 -0300

    Initial Commit

---
+++ BoardAppsTest/BoardAppsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardAppsTestApp.swift
+//  BoardAppsTest
+//
+//  Created by Alex on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BoardAppsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
